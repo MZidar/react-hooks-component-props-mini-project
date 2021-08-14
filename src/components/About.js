24596405c384bb5a0634fdd2cec1d515a2089894
Michael Zidar <mzidar@gmail.com>
2021-08-14 15:26:40 -0400

    commit tree de0582f4c492bae77cacd439374cdb4544e30f50

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+function About(){return "hi"}
+
+
+
+
+export default About;
---
+++ src/components/ArticleList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Article from "./Article"
+
+
+function ArticleList(){return "hi"}
+
+
+
+
+export default ArticleList;
